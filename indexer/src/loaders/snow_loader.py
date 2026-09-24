28d6b5310329ea4eef1e015b8cf0ca5051e27bd8
Sebastian.Berger <sebastian.berger@muenchen.de>
2026-09-24 14:59:24 +0200

    fix typo

--- indexer/src/loaders/snow_loader.py
+++ indexer/src/loaders/snow_loader.py
@@ -41,7 +41,7 @@ class SnowLoader:
         self._page_size = config.servicenow_page_size
         self._languages = config.languages_list
         self._source_id = config.servicenow_source_id
-        self._protal_url = config.service_portal_url
+        self._portal_url = config.service_portal_url
         self._session = requests.Session()
         self._session.proxies.update(config.proxies)
 
@@ -164,7 +164,7 @@ class SnowLoader:
                     logger.warning("Skipping ServiceNow article %s because it has no content", sys_id)
                     continue
 
-                source = f"{self._protal_url}{article.get('link')}"
+                source = f"{self._portal_url}{article.get('link')}"
 
                 documents.append(
                     Document(
